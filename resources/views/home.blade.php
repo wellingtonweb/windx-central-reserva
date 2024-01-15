322e3f563089096b110f64cb8c5968fa624e880b
wellingtonweb <55607138+wellingtonweb@users.noreply.github.com>
2024-01-15 10:42:19 -0300

    Display toolbar responsive layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,21 +13,13 @@
                         </h5>
                     </div>
                 </div>
-                <div class="container-actions-buttons d-flex justify-content-start mb-3 flex-wrap ">
+                <div class="container-actions-buttons d-flex justify-content-start mb-5 flex-wrap ">
                     <div class="action-button animate__animated animate__fadeIn animate__delay-2s">
                         <a href="{{route('central.contract')}}" class="text-custom">
                             <div class="w-100 h-100 card service-wrapper rounded border-0 shadow p-4">
-                                <div class="container-icon w-100  d-flex justify-content-center">
+                                <div class="container-icon w-100 d-flex justify-content-center">
                                     <div class="icon text-center text-custom h1">
                                         <span class="uim-svg pl-1">
-{{--                                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"--}}
-{{--                                                 class="bi bi-file-text" viewBox="0 0 18 18" width="1.2em">--}}
-{{--                                              <path--}}
-{{--                                                  d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>--}}
-{{--                                              <path--}}
-{{--                                                  d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>--}}
-{{--                                            </svg>--}}
-
                                             <svg xmlns="http://www.w3.org/2000/svg" style="width: 3.4rem" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
                                               <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
                                               <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
@@ -56,37 +48,19 @@
                     </div>
                     <div class="action-button animate__animated animate__fadeIn animate__delay-2s">
                         <a href="{{route('central.payment')}}" class="text-custom">
-                            <div class="w-100 h-100 card service-wrapper rounded border-0 shadow p-4">
-                                <div class="container-icon w-100  d-flex justify-content-center">
+                            <div class="w-100 h-100 card service-wrapper rounded border-0 shadow p-4 ">
+                                <div class="container-icon w-100 d-flex justify-content-center bg-primary">
                                     <div class="icon text-center text-custom">
-
-                                    <span class="uim-svg">
                                         <span class="uim-svg">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="2em" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
                                               <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z"/>
                                             </svg>
-
-
-
-
-
-{{--                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"--}}
-{{--                                             class="bi bi-cash-coin" viewBox="0 0 18 18" width="1.2em">--}}
-{{--                                          <path fill-rule="evenodd"--}}
-{{--                                                d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z"/>--}}
-{{--                                          <path--}}
-{{--                                              d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z"/>--}}
-{{--                                          <path--}}
-{{--                                              d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z"/>--}}
-{{--                                          <path--}}
-{{--                                              d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z"/>--}}
-{{--                                        </svg>--}}
-                                    </span>
+                                        </span>
                                     </div>
                                 </div>
-                                <div class="content mt-lg-4 mt-0">
+                                <div class="content mt-lg-4 mt-0 bg-secondary text-center">
                                     <h4 class="title">Pagamento</h4>
-                                    <p class="text-muted mt-3 mb-0 pl-2">Pagar faturas usando PIX, PICPAY, CRÉDITO,
+                                    <p class="text-muted mt-3 mb-0 ">Pagar faturas usando PIX, PICPAY, CRÉDITO,
                                         DÉBITO ou baixar uma 2ª via.</p>
                                 </div>
                                 <div class="big-icon h1 text-custom">
@@ -915,6 +889,7 @@
 
             .action-button {
                 max-width: calc(50% - 20px);
+                min-width: calc(140px - 20px);
             }
 
             .action-button .icon {
@@ -924,7 +899,7 @@
             .action-button .content {
                 position: relative;
                 width: 140px !important;
-                margin-left: -17px;
+                /*margin-left: -17px;*/
             }
 
             .action-button .content h4 {
@@ -941,6 +916,15 @@
             /*.container {*/
             /*    margin-bottom: 0 !important;*/
             /*}*/
+
+            /* modification menu mobile */
+            .mastfoot {
+                display: none !important;
+            }
+
+            .nav-web {
+                display: none !important;
+            }
         }
 
         @media (min-width: 576px) and (max-width: 767.98px) {
@@ -948,11 +932,34 @@
                 max-width: calc(50% - 30px) !important;
             }
 
+            .mastfoot {
+                display: none !important;
+            }
+
+            .nav-web {
+                display: none !important;
+            }
+
+            .nav-mobile {
+                display: block !important;
+            }
         }
 
         @media (min-width: 768px) and (max-width: 991.98px) {
             .action-button {
                 max-width: calc(25% - 1.2rem) !important;
+            }
+
+            .mastfoot {
+                display: none !important;
+            }
+
+            .nav-web {
+                display: none !important;
+            }
+
+            .nav-mobile {
+                display: block !important;
             }
         }
 
@@ -960,11 +967,19 @@
             .action-button {
                 max-width: calc(25% - 1.2rem) !important;
             }
+
+            .nav-mobile {
+                display: none !important;
+            }
         }
 
         @media (min-width: 1200px) {
             .action-button {
                 max-width: calc(25% - 1.2rem) !important;
+            }
+
+            .nav-mobile {
+                display: none !important;
             }
         }
 
@@ -1225,8 +1240,6 @@
             font-size: inherit;
             fill: var(--uim-color, currentColor);
         }
-
-
 
     </style>
 @endsection
--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,5 +1,5 @@
 <header id="header" class="header">
-    <div class="nav-web animate__animated animate__fadeInDown animate__delay-1s d-none">
+    <div class="nav-web animate__animated animate__fadeInDown animate__delay-1s ">
         <nav class="navbar sidebarNavigation p-lg-3 p-md-3 p-sm-0" data-sidebarClass="navbar-dark- bg-dark-">
             <div class="container-fluid">
                 <a class="navbar-brand {{ Route::currentRouteName() === 'central.home' ? 'd-none' : '' }}" href="#" onclick="javascript:history.back()">
@@ -161,8 +161,6 @@
                 <a href="{{ route('central.contract') }}" class="click-loader close-menu
                 {{ Route::currentRouteName() === 'central.contract' ? 'active' : '' }}">
                     <div class="bottom-nav-link {{ Route::currentRouteName() === 'central.contract' ? 'icon-active' : '' }}">
-{{--                        <i class="fa fa-list"></i>--}}
-{{--                        <i class="far fa-file-alt"></i>--}}
                         <i class="fas fa-file-alt"></i>
                         <span>Contrato</span>
                     </div>
@@ -173,9 +171,6 @@
             {{ Route::currentRouteName() === 'central.payment' ? 'active' : '' }}">
                     <div class="bottom-nav-link {{ Route::currentRouteName() === 'central.payment' ? 'icon-active' : '' }}">
                         <i class="fas fa-dollar-sign"></i>
-{{--                        <svg style="font-weight: 900" xmlns="http://www.w3.org/2000/svg" fill="" class="bi bi-currency-dollar" viewBox="0 0 16 16">--}}
-{{--                            <path style="fill: darkgrey" d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z"/>--}}
-{{--                        </svg>--}}
                         <span>Pagamento</span>
                     </div>
                 </a>
@@ -183,7 +178,6 @@
             <div class="bottom-nav-item">
                 <div class="bottom-nav-link">
                     <i class="fab fa-whatsapp"></i>
-{{--                    <i class="fab fa-whatsapp-square"></i>--}}
                     <span>WhatsApp</span>
                 </div>
             </div>
